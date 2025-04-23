bf4e54dcafff700f579ba662caf69dc46e8285ad
zyq <yezipiaomu@gmail.com>
2025-04-23 14:16:25 +0800

    add camel change and image analysis

--- examples/run.py
+++ examples/run.py
@@ -28,6 +28,7 @@ from camel.toolkits import (
 from camel.types import ModelPlatformType, ModelType, TaskType
 from camel.logger import set_log_level
 from camel.societies import RolePlaying
+from .role_play import RolePlaying
 
 from owl.utils import run_society, DocumentProcessingToolkit
 
@@ -114,7 +115,7 @@ def construct_society(question: str) -> RolePlaying:
     ]
 
     user_profile = """用户的照片存放在以下路径:
-    "/Users/zhuyuyao/Documents/llm应用/联想demo/美国旅行"
+    "/Users/zyq/workspace/manus_project/lenovo/data"
 """
 
     # Configure agent roles and parameters
@@ -176,7 +177,7 @@ def main():
     # default_task = "Navigate to Amazon.com and identify one product that is attractive to coders. Please provide me with the product name and price. No need to verify your answer."
     # default_task = "搜索大疆action 5pro的价格"
     default_task = (
-        "查看目录\"/Users/zhuyuyao/Documents/llm应用/联想demo/旅行\"下的照片，根据图片内容生成一个旅游游记markdown, 存储在当前目录的trip_log_mcp.md中。markdown内容包含图片(图片引用使用绝对路径)以及对应的内容描述，整体构成一个完整旅行叙事。"
+        "查看目录\"/Users/zyq/workspace/manus_project/lenovo/data\"下的照片，根据图片内容生成一个旅游游记markdown, 存储在当前目录的trip_log_mcp.md中。markdown内容包含图片(图片引用使用绝对路径)以及对应的内容描述，整体构成一个完整旅行叙事。"
     )
 
     # Override default task if command line argument is provided
