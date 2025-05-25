4f1f320d487322d329b9bb3703d89fd3e8d9cea9
Yuhang Zhou <86864241+Ralph-Zhou@users.noreply.github.com>
2025-05-25 15:40:01 +0800

    Update run_gaia_workforce.py

--- run_gaia_workforce.py
+++ run_gaia_workforce.py
@@ -212,7 +212,7 @@ def evaluate_on_gaia():
     LEVEL = 1
     on="valid"
     SAVE_RESULT = True
-    MAX_TRIES = 1
+    MAX_TRIES = 3
     
     SAVE_RESULT_PATH = f"results/workforce/workforce_{LEVEL}_pass{MAX_TRIES}_gpt4o.json"
     test_idx = [1]
